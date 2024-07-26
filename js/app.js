7f29b7695fbbc338909fbd31728f9b505e3b08a4
Mirzoidov Mirodil <mirzoidovmirodil@gmail.com>
2024-07-26 13:29:44 +0500

    Desktop version completed

---
+++ js/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('#popup-close');
+const cards = document.querySelectorAll('.latest-news-item');
+
+function modal(){
+    popup.style.display = 'flex';
+    setTimeout(close, 10000); // Close the popup after 10 seconds
+    closeBtn.addEventListener("click", close);
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Escape") {
+            close();
+        }
+    });
+};
+
+function close() {
+    popup.style.display = 'none';
+};
+
+const navAnim = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".menu");
+    const navLinks = nav.querySelectorAll(".menu li");
+  
+    // Toggle nav
+    burger.addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+      //Animate Items
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkMove 0.5s ease forwards
+              ${index / 7 + 0.35}s`;
+        }
+      });
+  
+      // Animated Burger
+      burger.classList.toggle("toggle")
+    });
+  };
+  
+  navAnim();
